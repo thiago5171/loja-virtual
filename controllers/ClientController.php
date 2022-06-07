9ec89cfad866295e7b0d09c3a4a50b3c724db594
Thiago Gazaroli Cardoso <Tgazaroli@gmail.com>
2022-06-07 00:58:28 -0300

    add controller of client

--- controllers/ClientController.php
+++ controllers/ClientController.php
@@ -1,1 +1,63 @@
 <?php
+require_once 'model/Client.php';
+require_once 'model/ClientDAO.php';
+
+class ClientController
+{
+    private $client;
+    private $clientDAO;
+
+    function create()
+    {
+        $this->client = new Client();
+        $this->clientDAO = new ClientDAO();
+        $this->client->setName($_REQUEST["nome"]);
+        $this->client->setCpf($_REQUEST["cpf"]);
+        $this->client->setPassword($_REQUEST["password"]);
+        if ($this->clientDAO->create($this->client)) {
+            $_REQUEST['sucesso'] = true;
+            $_REQUEST['clietNome'] = $this->client->getName();
+            require_once 'view/client/create.php';
+        } else {
+            echo 'Erro ao criar cliente';
+        }
+    }
+
+    function fetch()
+    {
+        $this->clientDAO = new ClientDAO();
+        $_REQUEST['clientLista'] = $this->clientDAO->fetch();
+        require_once 'view/client/listar.php';
+
+    }
+
+    function getById()
+    {
+        $this->clientDAO = new ClientDAO();
+        $this->client = new Client();
+        $this->client->setId($_REQUEST["id"]);
+        if ($this->client = $this->clientDAO->getById($this->client->getId())) {
+
+            $_REQUEST['clienteExibir'] = $this->categoria;
+            require_once 'view/client/editar.php';
+        } else {
+            echo 'Erro ao retornar cliente';
+        }
+    }
+
+    function edit()
+    {
+        $this->clientDAO = new ClientDAO();
+        $this->client = new Client();
+        $this->client->setId($_REQUEST["id"]);
+        $this->client->setName($_REQUEST["nome"]);
+        $this->client->setCpf($_REQUEST["cpf"]);
+        $this->client->setPassword($_REQUEST["password"]);
+        if ($this->clientDAO->edit($this->client)) {
+            echo 'cliente editada com sucesso';
+        } else {
+            echo 'Erro ao editar cliente';
+        }
+    }
+}
+
--- model/ClientDAO.php
+++ model/ClientDAO.php
@@ -19,39 +19,44 @@ class ClientDAO
         $stmt->bindValue(":name", $client->getName());
         $stmt->bindValue(":cpf", $client->getCpf());
         $stmt->bindValue(":password", $client->getPassword());
-        if($stmt->execute()){
+        if ($stmt->execute()) {
             return true;
-        }else{
+        } else {
             print_r("Erro ao cadastrar cliente");
             return false;
         }
     }
 
-    function fetch(){
+    function fetch()
+    {
         $sql = "SELECT * FROM cliente";
-        $stmt =$this->conexao->prepare($sql);
+        $stmt = $this->conexao->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll();
     }
+
     function getById($id)
     {
         $sql = "SELECT * FROM cliente WHERE id = :id";
         $stmt = $this->conexao->prepare($sql);
-        $stmt->bindValue(":id",$id);
+        $stmt->bindValue(":id", $id);
         $stmt->execute();
         return $stmt->fetchAll();
     }
 
     function edit($client)
     {
-        $sql = "UPDATE cliente SET nome = :name, cpf = :cpf, senha = :password";
+        $sql = "UPDATE cliente SET nome = :name, cpf = :cpf, senha = :password WHERE id = :id";
         $stmt = $this->conexao->prepare($sql);
         $stmt->bindValue(":name", $client->getName());
         $stmt->bindValue(":cpf", $client->getCpf());
+        $stmt->bindValue(":id", $client->getId());
         $stmt->bindValue(":password", $client->getPassword());
-        return  $stmt->execute();
+        return $stmt->execute();
     }
-    function delete($client){
+
+    function delete($client)
+    {
         $stmt = $this->conexao->prepare("DELETE FROM cliente  WHERE id = :id");
         $stmt->bindValue(":id", $client->getId());
         return $stmt->execute();
